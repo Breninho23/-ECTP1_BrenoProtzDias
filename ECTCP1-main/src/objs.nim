15915e9346743208f09a41b34d882e88349e1131
Breninho23 <60906215+Breninho23@users.noreply.github.com>
2021-03-30 23:06:54 -0300

    Add files via upload

---
+++ ECTCP1-main/src/objs.nim
@@ -0,0 +1,17 @@
+type
+  Fornecedor* = ref object of RootObj  
+    nomeFantasia*:string
+    cnpj*:string
+    endereco*:string 
+    cnae*:string 
+
+  Cliente* = ref object of RootObj
+    nomeFantasia*:string 
+    cnpj*:string 
+    endereco*:string  
+    categoria*:string
+
+  Produto* = ref object of RootObj
+    codigo*:int 
+    descricao*:string 
+    valor*:float 
